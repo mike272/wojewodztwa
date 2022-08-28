49750e2ffe169aa324ce9bc074c912bfc8e0b9be
mike272 <babinmichal@gmail.com>
2022-08-28 14:44:25 +0400

    Initial Commit

---
+++ wojewodztwa/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  wojewodztwa
+//
+//  Created by Wojciech Babinski on 28/08/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
