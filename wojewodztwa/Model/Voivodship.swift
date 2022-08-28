4b24cf4a959e4bc89cec38d6a9beba6f94b2b0db
mike272 <babinmichal@gmail.com>
2022-08-28 14:47:46 +0400

    created model of voivodship

---
+++ wojewodztwa/Model/Voivodship.swift
@@ -0,0 +1,18 @@
+//
+//  Voivodship.swift
+//  wojewodztwa
+//
+//  Created by Wojciech Babinski on 28/08/2022.
+//
+
+import Foundation
+
+struct Voivodship{
+    var name:String
+    var imageName:String
+    
+    init(_name:String, _imageName:String ) {
+        self.name = _name
+        self.imageName = _imageName
+    }
+}
